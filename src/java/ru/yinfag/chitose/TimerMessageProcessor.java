d2c240c02bea016e842ec25b60cadea7f6561d12
Yanus Poluektovich <ypoluektovich@gmail.com>
2012-03-09 22:41:44 +0400

    Code cleanup for Timer message processor

--- src/java/ru/yinfag/chitose/TimerMessageProcessor.java
+++ src/java/ru/yinfag/chitose/TimerMessageProcessor.java
@@ -1,27 +1,28 @@
 package ru.yinfag.chitose;
 
-import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.XMPPException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.HashMap;
-import java.util.Map;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TimerMessageProcessor implements MessageProcessor {
-	
+
+	private static final Pattern p = Pattern.compile(
+			".*?(?:Chitose|[Чч]итосе).*?напомни.*?о \"(.+?)\" через ([1-9][0-9]*).*?(?:минут[ыу]?)"
+	);
+
 	private final MultiUserChat muc;
-	
+
 	TimerMessageProcessor(final MultiUserChat muc) {
 		this.muc = muc;
 	}
-	
-	private static final Pattern p = Pattern.compile(
-		".*?(?:(?:Chitose)|(?:[Чч]итосе)).*?напомни.*?о \"(.+?)\" через ([0-9]+).*?(?:(?:минут)|(?:минуты)|(?:минуту))"
-	);
-	
+
 	private final Map<String, Timer> timers = new HashMap<>();
 	
 	@Override
@@ -32,45 +33,40 @@ public class TimerMessageProcessor implements MessageProcessor {
 			return null;
 		}
 		
-		final String userNick = MessageProcessorUtils.getUserNick(message);
-		
-		String timeMinute = m.group(2);
-		final String sage = m.group(1);	
-		long timeMinuteLong = 0;
+		final String sage = m.group(1);
+
+		final String timeMinute = m.group(2);
+		final long timeMinuteLong;
 		try {
-			timeMinuteLong =  Long.parseLong(timeMinute.trim());
+			timeMinuteLong = Long.parseLong(timeMinute.trim());
 		} catch (NumberFormatException e) {
+			// we could reply with an error message
+			// but our regex shouldn't even allow this to be thrown
 			throw new MessageProcessingException(e);
 		}
+
 		final String pseudoJid = message.getFrom();
-		boolean oldTimer = timers.containsKey(pseudoJid);
+		final boolean oldTimer = timers.containsKey(pseudoJid);
 		if (oldTimer) {
 			timers.remove(pseudoJid).cancel();
-			
 		}
-		Timer timer = new Timer();
+
+		final String userNick = MessageProcessorUtils.getUserNick(message);
+
+		final Timer timer = new Timer();
 		timers.put(pseudoJid, timer);
-		TimerTask task = new TimerTask() {
-			public void run()
-			{
+		final TimerTask task = new TimerTask() {
+			public void run() {
 				timers.remove(pseudoJid);
 				try {
-					muc.sendMessage(userNick + ": Напоминаю! " + "\n" + sage);
+					muc.sendMessage(userNick + ": Напоминаю!\n" + sage);
 				} catch (XMPPException e) {
 					e.printStackTrace();
 				}
 			}
 		};
-		if (timeMinuteLong != 0 && oldTimer ) {
-			long time = timeMinuteLong * 60000;
-			timer.schedule(task, time);
-			return userNick + ": таймер изменён!";
-		} else if (timeMinuteLong != 0) {
-			long time = timeMinuteLong * 60000;
-			timer.schedule(task, time);
-			return userNick + ": окей!";
-		} else {
-			return null;
-		}
+
+		timer.schedule(task, timeMinuteLong * 60000);
+		return userNick + ": " + (oldTimer ? "таймер изменён!" : "окей!");
 	}
 }
