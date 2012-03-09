a63435e22087d30dce2c514d9ac9eefad77cdf3d
Yanus Poluektovich <ypoluektovich@gmail.com>
2012-03-10 01:32:06 +0400

    Arguably better code.

--- src/java/ru/yinfag/chitose/HelpMessageProcessor.java
+++ src/java/ru/yinfag/chitose/HelpMessageProcessor.java
@@ -7,23 +7,21 @@ import java.util.regex.Pattern;
 
 public class HelpMessageProcessor implements MessageProcessor {
 
-	private static final Pattern p = Pattern.compile(
-		".*?Chitose.*?(?:[Хх]елп|[Hh]elp)"
-		);
+	private static final Pattern PATTERN = Pattern.compile(
+			".*?Chitose.*?(?:[Хх]елп|[Hh]elp)"
+	);
 	
-	private static final String help =  
-		" \n" +
-		"Кидаем кубики: Читосе, кинь 2d6\n" +
-		"Постим няшек: Читосе запости няшку\n" +
-		"Постим определённую няшку: Читосе, запости misaka_mikoto\n" +
-		"Напоминаем о том, что неюка всё зафейлит: Читосе, напомни о \"Неюка всё зафелит\" через 6 минут \n" +
-		"Ищем мультики на вротарте: Читосе расскажи про \"НАРУТО\"";
-	
-	
+	private static final String HELP_MESSAGE =
+		"\nКидаем кубики: Читосе, кинь 2d6" +
+				"\nПостим няшек: Читосе запости няшку" +
+				"\nПостим определённую няшку: Читосе, запости misaka_mikoto" +
+				"\nНапоминаем о том, что неюка всё зафейлит: Читосе, напомни о \"Неюка всё зафейлит\" через 6 минут" +
+				"\nИщем мультики на вротарте: Читосе расскажи про \"НАРУТО\"";
+
 	@Override
 	public CharSequence process(final Message message) throws MessageProcessingException {
-		Matcher m = p.matcher(message.getBody());
-		return (!m.find()) ? null : help;
+		final Matcher m = PATTERN.matcher(message.getBody());
+		return (m.find()) ? HELP_MESSAGE : null;
 	}
 }
 		
