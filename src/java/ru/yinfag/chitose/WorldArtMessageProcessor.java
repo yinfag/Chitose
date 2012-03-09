cf40170e4f5c2f2e8c5fd7d97c96581b4159d5ff
Yanus Poluektovich <ypoluektovich@gmail.com>
2012-03-09 22:47:37 +0400

    Cleaned up World-Art processor. A lot. And it might still need more.

--- src/java/ru/yinfag/chitose/WorldArtMessageProcessor.java
+++ src/java/ru/yinfag/chitose/WorldArtMessageProcessor.java
@@ -5,154 +5,168 @@ import org.jivesoftware.smack.packet.Message;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.net.URLEncoder;
-import java.io.UnsupportedEncodingException;
 
 public class WorldArtMessageProcessor implements MessageProcessor {
 	
-	private static final Pattern p = Pattern.compile(
+	private static final Pattern COMMAND_PATTERN = Pattern.compile(
 			"(?:(?:Chitose)|(?:[Чч]итосе)).*?расскажи.*?про \"(.+?)\""
 	);
-	private static final String p1 = "отсортировано по дате выхода";
-	private static final Pattern p2 = Pattern.compile("'estimation'>(.+?)&nbsp");
-	private static final Pattern p3 = Pattern.compile("Краткое содержание:.*?class='review'>(.+?)</p>");
-	private static final String p4 = "<b>Раздел &laquo;анимация&raquo;";
-	private static final Pattern p5 = Pattern.compile("animation/animation.php\\?id=(\\d+)");
-	private static final String p6 = "<meta http-equiv='Refresh' content='0;";
+
+	private static final String MULTIPLE_TITLES_INDICATOR = "отсортировано по дате выхода";
+
+	private static final Pattern MULTIPLE_TITLE_LIST_ENTRY_PATTERN = Pattern.compile("'estimation'>(.+?)&nbsp");
+
+	private static final Pattern SYNOPSIS_PATTERN = Pattern.compile("Краткое содержание:.*?class='review'>(.+?)</p>");
+
+	private static final String MULTIPLE_SECTIONS_INDICATOR = "<b>Раздел &laquo;анимация&raquo;";
+
+	private static final Pattern TITLE_ENTRY_PATTERN = Pattern.compile("animation/animation.php\\?id=(\\d+)");
+
+	private static final String META_REFRESH = "<meta http-equiv='Refresh' content='0;";
 	
 	@Override
 	public CharSequence process(final Message message) throws MessageProcessingException {
-		Matcher m = p.matcher(message.getBody());
-		if (!m.matches()) {
+		final String title = getTitle(message.getBody());
+		if (title == null) {
 			return null;
 		}
-		
-		final String userNick = MessageProcessorUtils.getUserNick(message);
-		
-		URL worldart;
-		String title = "";
+
+		final URL worldart;
 		try {
-			title = URLEncoder.encode(m.group(1), "CP1251");
-		} catch (UnsupportedEncodingException e){
-			throw new MessageProcessingException(e);
-		}
-		try {
-			worldart = new URL("http://www.world-art.ru/search.php?public_search=" +title+"&global_sector=animation");
+			worldart = new URL(
+					"http://www.world-art.ru/search.php?public_search=" +
+							title +
+							"&global_sector=animation"
+			);
 		} catch (MalformedURLException e) {
 			throw new MessageProcessingException(e);
 		}
+
 		boolean multipleTitleVariants = false;
-		boolean animationPresent = false;
+		boolean multipleSections = false;
 		try (BufferedReader in = new BufferedReader(new InputStreamReader(worldart.openStream(), "cp1251"))) {
 			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
-				if (inputLine.contains(p1)) {
+				if (inputLine.contains(MULTIPLE_TITLES_INDICATOR)) {
 					multipleTitleVariants = true;
 				}
-				if (inputLine.contains(p4)) {
-						animationPresent = true;
+				if (inputLine.contains(MULTIPLE_SECTIONS_INDICATOR)) {
+					multipleSections = true;
 				}
-				if (multipleTitleVariants && animationPresent) {
+				if (multipleTitleVariants && multipleSections) {
 					break;
 				}
 			}
 		} catch (IOException e) {
 			throw new MessageProcessingException(e);
 		}
-		System.out.format("MTV: %b; AP: %b%n", multipleTitleVariants, animationPresent);
+
+		/*
+		 * Examples:
+		 * "Air" gives several titles and 2 sections (animation and clips)
+		 */
+
+		final String answer;
 		if (multipleTitleVariants) {
+			final List<String> titles = new ArrayList<>();
 			try (BufferedReader in = new BufferedReader(new InputStreamReader(worldart.openStream(), "cp1251"))) {
-				final List<String> titles = new ArrayList<String>();
 				String inputLine;
 				while ((inputLine = in.readLine()) != null) {
-					Matcher m2 = p2.matcher(inputLine);
-					while (m2.find()) {
-						final String postTitles = m2.group(1);
+					final Matcher matcher = MULTIPLE_TITLE_LIST_ENTRY_PATTERN.matcher(inputLine);
+					while (matcher.find()) {
+						final String postTitles = matcher.group(1);
 						titles.add(postTitles);
 					}
 				}
-				String titleList = Utils.join(titles, ", ");
-				if (titles.size() > 10) {
-					return "Найдено "+titles.size()+" результатов, попробуй уточнить свой запрос.";
-				} else {
-					return "Возможно ты имел ввиду: "+titleList;
-				}
 			} catch (IOException e) {
 				throw new MessageProcessingException(e);
 			}
-		} else if (animationPresent) {
+			if (titles.size() > 10) {
+				answer = "Найдено " + titles.size() + " результатов, попробуй уточнить свой запрос.";
+			} else {
+				final String titleList = Utils.join(titles, ", ");
+				answer = "Возможно, ты имел в виду: " + titleList;
+			}
+		} else if (multipleSections) {
+			String titleID = null;
 			try (BufferedReader in = new BufferedReader(new InputStreamReader(worldart.openStream(), "cp1251"))) {
 				String inputLine;
-				String titleID = null;
-				URL worldart1;
 				while ((inputLine = in.readLine()) != null) {
-					Matcher m5 = p5.matcher(inputLine);
-					if (m5.find()) {
-						titleID = m5.group(1);
+					final Matcher matcher = TITLE_ENTRY_PATTERN.matcher(inputLine);
+					if (matcher.find()) {
+						titleID = matcher.group(1);
+						break;
 					}
 				}
-				try {
-					worldart1 = new URL("http://www.world-art.ru/animation/animation.php?id="+titleID);
-				} catch (MalformedURLException e) {
-					throw new MessageProcessingException(e);
-				}
-				String synopsis1 = "Похоже описание отсутствует. Щто поделать, десу.";
-				try (BufferedReader in1 = new BufferedReader(new InputStreamReader(worldart1.openStream(), "cp1251"))) {
-					String inputLine1;
-					while ((inputLine1 = in1.readLine()) != null) {
-						Matcher m3 = p3.matcher(inputLine1);
-						if (m3.find()) {
-							synopsis1 = m3.group(1);
-						}
-					}
-				}
-				return synopsis1.replaceAll("\\<br\\>", "\n") + "\n" +worldart1;
 			} catch (IOException e) {
 				throw new MessageProcessingException(e);
 			}
+			answer = getSynopsis(titleID);
 		} else {
+			String titleID = null;
 			try (BufferedReader in = new BufferedReader(new InputStreamReader(worldart.openStream(), "cp1251"))) {
 				String inputLine;
-				URL worldart1;
-				String titleID = null;
 				while ((inputLine = in.readLine()) != null) {
-					if (inputLine.contains(p6)) {
-						Matcher m5 = p5.matcher(inputLine);
-						if (m5.find()) {
-							titleID = m5.group(1);
+					if (inputLine.contains(META_REFRESH)) {
+						final Matcher matcher = TITLE_ENTRY_PATTERN.matcher(inputLine);
+						if (matcher.find()) {
+							titleID = matcher.group(1);
+							break;
 						}
 					}
-				}
-				try {
-					worldart1 = new URL("http://www.world-art.ru/animation/animation.php?id="+titleID);
-				} catch (MalformedURLException e) {
-					throw new MessageProcessingException(e);
-				}
-				String synopsis = "Нет такого мультфильма!";
-				try (BufferedReader in1 = new BufferedReader(new InputStreamReader(worldart1.openStream(), "cp1251"))) {
-					String inputLine1;
-					while ((inputLine1 = in1.readLine()) != null) {
-						Matcher m3 = p3.matcher(inputLine1);
-						if (m3.find()) {
-							synopsis = m3.group(1);
-						}
-					}
-				}
-				if (titleID == null) {
-					return synopsis;
-				} else {
-					return synopsis.replaceAll("\\<br\\>", "\n") + "\n" +worldart;
 				}
 			} catch (IOException e) {
 				throw new MessageProcessingException(e);
-			} 
+			}
+			answer = getSynopsis(titleID);
+		}
+
+		return MessageProcessorUtils.getUserNick(message) + ": " + answer;
+	}
+	
+	private String getSynopsis(final String titleId) throws MessageProcessingException {
+		if (titleId == null) {
+			return "Нет такого мультфильма!";
+		}
+		final URL url;
+		try {
+			url = new URL("http://www.world-art.ru/animation/animation.php?id=" + titleId);
+		} catch (MalformedURLException e) {
+			throw new MessageProcessingException(e);
+		}
+		String synopsis = "Похоже, описание отсутствует. Щто поделать, десу.";
+		try (final BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "cp1251"))) {
+			String line;
+			while ((line = in.readLine()) != null) {
+				final Matcher matcher = SYNOPSIS_PATTERN.matcher(line);
+				if (matcher.find()) {
+					synopsis = "\n" + matcher.group(1);
+					break;
+				}
+			}
+		} catch (IOException e) {
+			throw new MessageProcessingException(e);
+		}
+		return synopsis.replaceAll("<br>", "\n").replaceAll("\n{2,}", "\n") + "\n" + url;
+	}
+	
+	private String getTitle(final String message) throws MessageProcessingException {
+		final Matcher commandMatcher = COMMAND_PATTERN.matcher(message);
+		if (!commandMatcher.matches()) {
+			return null;
+		}
+		try {
+			return URLEncoder.encode(commandMatcher.group(1), "CP1251");
+		} catch (UnsupportedEncodingException e){
+			throw new MessageProcessingException(e);
 		}
 	}
 }
