e867810ac15b86d6c1859ffbd5fb9c3fa8741416
yinfag <makfirest@gmail.com>
2012-06-11 22:51:01 +0600

    smooch plugin

---
+++ src/java/ru/yinfag/chitose/plugin/smooch/SmoochPlugin.java
@@ -0,0 +1,22 @@
+package ru.yinfag.chitose.plugin.smooch;
+
+import ru.yinfag.chitose.MessageProcessorPlugin;
+
+public class SmoochPlugin implements MessageProcessorPlugin {
+	
+	
+	@Override
+	public void processMessage() {
+		//...
+	}
+	
+	public void init() {
+		//...
+	}
+	
+	public void shutdown() {
+		//...
+	}
+	
+}
+
