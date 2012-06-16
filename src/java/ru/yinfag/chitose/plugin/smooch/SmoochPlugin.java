d132e79a04d6b60b05e2cedeaf427913458a3314
yinfag <makfirest@gmail.com>
2012-06-16 22:07:49 +0600

    ещё один метод в чмокплагине

--- src/java/ru/yinfag/chitose/plugin/smooch/SmoochPlugin.java
+++ src/java/ru/yinfag/chitose/plugin/smooch/SmoochPlugin.java
@@ -1,8 +1,10 @@
 package ru.yinfag.chitose.plugin.smooch;
 
 import ru.yinfag.chitose.MessageProcessorPlugin;
+import ru.yinfag.chitose.MessageSenderPlugin;
+import ru.yinfag.chitose.MessageSender;
 
-public class SmoochPlugin implements MessageProcessorPlugin {
+public class SmoochPlugin implements MessageProcessorPlugin, MessageSenderPlugin {
 	
 	
 	@Override
@@ -15,6 +17,10 @@ public class SmoochPlugin implements MessageProcessorPlugin {
 	}
 	
 	public void shutdown() {
+		//...
+	}
+	
+	public void setMessageSender(final MessageSender sender) {
 		//...
 	}
 	
