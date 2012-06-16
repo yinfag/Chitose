8a3980ff8085bc49793c0f9045a73aa48fa8a49a
yinfag <makfirest@gmail.com>
2012-06-16 21:58:14 +0600

    MessageSender & MessageSenderPlugin

---
+++ src/java/ru/yinfag/chitose/MessageSender.java
@@ -0,0 +1,8 @@
+package ru.yinfag.chitose;
+
+
+public interface MessageSender {
+	
+	void sendToConference(final String conference, final String message);
+	
+}
---
+++ src/java/ru/yinfag/chitose/MessageSenderPlugin.java
@@ -0,0 +1,8 @@
+package ru.yinfag.chitose;
+
+
+public interface MessageSenderPlugin extends Plugin {
+
+	void setMessageSender(final MessageSender sender);
+
+}
